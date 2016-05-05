838e054d2f8b3d0ebe13600bddf013cc49d6bf03
rameshr <rameshr@Newt-Pk044.newtglobal.chn.local>
2016-05-05 10:53:11 +0530

    This is the first commit for my project

---
+++ GitHubSampleProject/src/com/newtglobal/github/HelloGitHub.java
@@ -0,0 +1,12 @@
+package com.newtglobal.github;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.out.println("Welcome to GitHUb");
+
+	}
+
+}
